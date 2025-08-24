d3dccbcf3ab47e40bb332c0d63f6e6c504a1f205
baraviz <bar.avizemer@gmail.com>
2025-08-24 17:22:05 +0300

    Try Check contion to team members

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ export function HomePage() {
     return (
         <section className="home">
             <h1>Home sweet Home!!!!!!!!!</h1>
+            <h2>Its me Bar!!!!!!!!!</h2>
         </section >
     )
 }
